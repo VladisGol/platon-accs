c6772090550b0fe0fb6ea341c66185f8edfa5b52
vladisgol <vladisgol@2acc3764-da52-0410-91d2-b5ef238ec21c>
2009-04-01 08:22:51 +0000

    Исправлена ошибка копирования атрибутов LNK на 0 объект в процедуре Fork. Сделано блокирование фрейма прагм для ветки NSI

--- EssClasses/rescopy.cpp
+++ EssClasses/rescopy.cpp
@@ -14,18 +14,18 @@ Lesser General Public License for more details.
 You should have received a copy of the GNU Lesser General Public
 License along with this library; if not, write to the Free Software
 Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
-Contacts: e-mail vladisgol@rambler.ru 
+Contacts: e-mail vladisgol@rambler.ru
 
 Ядро системы учета "Платон".
-Авторами программы являются Борисенков Сергей Александрович и Головырин Владислав Владимирович, 2005-2007г. 
-Данная библиотека является свободным программным обеспечением. Вы вправе распространять ее и/или модифицировать 
-в соответствии с условиями версии 2.1 Стандартной Общественной Лицензии Ограниченного Применения GNU, 
-опубликованной Free Software Foundation. 
-Мы распространяем эту программу в надежде на то, что она будет вам полезной, 
-однако НЕ ПРЕДОСТАВЛЯЕМ НА НЕЕ НИКАКИХ ГАРАНТИЙ, в том числе ГАРАНТИИ ТОВАРНОГО СОСТОЯНИЯ ПРИ ПРОДАЖЕ и 
-ПРИГОДНОСТИ ДЛЯ ИСПОЛЬЗОВАНИЯ В КОНКРЕТНЫХ ЦЕЛЯХ. 
-Для получения более подробной информации ознакомьтесь со Стандартной Общественной Лицензией Ограниченного 
-Применений GNU.  
+Авторами программы являются Борисенков Сергей Александрович и Головырин Владислав Владимирович, 2005-2007г.
+Данная библиотека является свободным программным обеспечением. Вы вправе распространять ее и/или модифицировать
+в соответствии с условиями версии 2.1 Стандартной Общественной Лицензии Ограниченного Применения GNU,
+опубликованной Free Software Foundation.
+Мы распространяем эту программу в надежде на то, что она будет вам полезной,
+однако НЕ ПРЕДОСТАВЛЯЕМ НА НЕЕ НИКАКИХ ГАРАНТИЙ, в том числе ГАРАНТИИ ТОВАРНОГО СОСТОЯНИЯ ПРИ ПРОДАЖЕ и
+ПРИГОДНОСТИ ДЛЯ ИСПОЛЬЗОВАНИЯ В КОНКРЕТНЫХ ЦЕЛЯХ.
+Для получения более подробной информации ознакомьтесь со Стандартной Общественной Лицензией Ограниченного
+Применений GNU.
 Связаться с авторами программы вы можете по e-mail vladisgol@rambler.ru
 */
 
@@ -92,11 +92,17 @@ RESCopy* RESCopy::Fork(long ID_ACTCopy_BelongFor)
                                 Destination->GetEAByNum(i)->SetDLLValue(Source->GetEAByNum(i)->GetDLLValue());
                                 break;
                         case ft_LinkHypotesis:
-                                Destination->GetEAByNum(i)->SetLink2HValue(Source->GetEAByNum(i)->GetLink2HValue());
+                        {
+								LNK_Value OneLNK=Source->GetEAByNum(i)->GetLink2HValue();
+								if(OneLNK.LinkTo>0) Destination->GetEAByNum(i)->SetLink2HValue(OneLNK);
                                 break;
+                        }
                         case ft_LinkPragma:
-                                Destination->GetEAByNum(i)->SetLink2PValue(Source->GetEAByNum(i)->GetLink2PValue());
+                        {
+								LNK_Value OneLNK=Source->GetEAByNum(i)->GetLink2PValue();
+								if(OneLNK.LinkTo>0) Destination->GetEAByNum(i)->SetLink2PValue(OneLNK);
                                 break;
+                        }
                         case ft_Security:
                                 break;
                         default:
