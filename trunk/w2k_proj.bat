1e7826b645f60239d16b70cf5c98a92b3c249774
vladisgol <vladisgol@2acc3764-da52-0410-91d2-b5ef238ec21c>
2010-03-21 09:34:29 +0000

    Добавлена форма Eidos studio и необходимые графические элементы для нее.

--- trunk/w2k_proj.bat
+++
@@ -1,7 +0,0 @@
-rem Скрипт генерирует "правильный" файл проекта для компиляции в среде Windows
-qmake -project
-copy project.inc platon-accs.tmp
-type platon-accs.pro >>platon-accs.tmp
-del platon-accs.pro
-ren platon-accs.tmp platon-accs.pro
-qmake
