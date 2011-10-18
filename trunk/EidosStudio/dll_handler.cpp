4c43acc1dda18ad8c4c06c49da43b94839badf02
vladisgol <vladisgol@2acc3764-da52-0410-91d2-b5ef238ec21c>
2011-10-18 17:54:12 +0000

    Продолжены работы по форме работы с библиотеками

--- trunk/EidosStudio/dll_handler.cpp
+++ trunk/EidosStudio/dll_handler.cpp
@@ -7,70 +7,111 @@ mw_DLL_handler::mw_DLL_handler(QWidget *parent)
 {
         ui.setupUi(this);
 
-        DataClass* DTL=platon::GetDataModule(this);
+        DTL=platon::GetDataModule(this);
         this->DB=DTL->DB;
 
         QObject::connect(ui.action_exit, SIGNAL(activated()), this, SLOT(Exit()));
         ReadFormWidgetsAppearance();
         FillDLLGrid();
-
 }
 
 void mw_DLL_handler::FillDLLGrid()
 {
     //Процедура заполняет таблицу зарегистрированными в программе библиотеками, устанавливая текущим элементом элемент, переданный в параметре CurItem, и колонку переданную в параметре Num
 
-        ui.tableWidget->setSortingEnabled(false);
-        ui.tableWidget->clearContents(); //Очищаем внутреннее содержимое таблицы
+    bool is_file_process=true;  //Требуется обработка локальной папки с библиотеками
+/*    QString ProgDir=QDir::currentPath();
+    QDir Folder(ProgDir);
+    if(Folder.cd("DLL")==false)	//Вход в каталог не выполнен
+        is_file_process=false;*/
 
-        IBPP::Transaction TmpTR=IBPP::TransactionFactory(this->DB,IBPP::amWrite, IBPP::ilConcurrency, IBPP::lrWait);
-        IBPP::Statement TmpST=IBPP::StatementFactory(this->DB, TmpTR);
-        TmpTR->Start();
-        TmpST->Prepare("select * from GET_REGED_DLLS;");
-        TmpST->Execute();
-        int i=0;
-        while (i<1)
+    IBPP::Transaction TmpTR=IBPP::TransactionFactory(this->DB,IBPP::amWrite, IBPP::ilConcurrency, IBPP::lrWait);
+    IBPP::Statement TmpST=IBPP::StatementFactory(this->DB, TmpTR);
+
+    TmpTR->Start();
+    TmpST->Prepare("select count(GET_REGED_DLLS.DLL_NAME) Number_dll from GET_REGED_DLLS;");
+    TmpST->Execute();
+    long NumberDLLs;    //Определяем количество библиотек прописанных в базе
+    if(TmpST->Fetch()) TmpST->Get("Number_dll",(int32_t*)&NumberDLLs);
+
+    ui.tableWidget_reged_dll->setSortingEnabled(false);
+    ui.tableWidget_reged_dll->clearContents(); //Очищаем внутреннее содержимое таблицы
+
+    ui.tableWidget_reged_dll->setColumnCount(2);
+    ui.tableWidget_reged_dll->setRowCount(NumberDLLs);
+
+    QTableWidgetItem *__vhi1 = new QTableWidgetItem();
+    __vhi1->setText(tr("Имя библиотеки"));
+    ui.tableWidget_reged_dll->setHorizontalHeaderItem(0, __vhi1);
+    QTableWidgetItem *__vhi2 = new QTableWidgetItem();
+    __vhi2->setText(tr("MD5 сумма"));
+    ui.tableWidget_reged_dll->setHorizontalHeaderItem(1, __vhi2);
+
+    TmpTR->Start();
+    TmpST->Prepare("select * from GET_REGED_DLLS;");
+    TmpST->Execute();
+
+    int i=0;
+    while (TmpST->Fetch())
+    {
+        //Считываем значения текущей записи для библиотеки
+        std::string DLL_Name;
+        std::string MD5_SUMM;
+        TmpST->Get("DLL_NAME",DLL_Name);
+        TmpST->Get("MD5_SUMM",MD5_SUMM);
+
+        QTableWidgetItem *__hhi = new QTableWidgetItem();
+        __hhi->setText(QString::number(i+1));   //В качестве подписи выступает последовательный номер записи в базе
+        ui.tableWidget_reged_dll->setVerticalHeaderItem(i, __hhi);
+
+        QTableWidgetItem *__ti1 = new QTableWidgetItem();
+        __ti1->setText(QString::fromStdString(DLL_Name));
+        ui.tableWidget_reged_dll->setItem(i, 0, __ti1);
+        QTableWidgetItem *__ti2 = new QTableWidgetItem();
+        __ti2->setText(QString::fromStdString(MD5_SUMM));
+        ui.tableWidget_reged_dll->setItem(i, 1, __ti2);
+
+        if(is_file_process)
         {
-            std::string DLL_Name;
-            std::string MD5_SUMM;
-            TmpST->Get("DLL_NAME",DLL_Name);
-            TmpST->Get("MD5_SUMM",MD5_SUMM);
+            if(QFile::exists(QString::fromStdString(DLL_Name))) //Файл найден на диске
+            {
+                ui.action_dll_subscribe->setEnabled(true);
+                if(DTL->CalcFileMD5(QString::fromStdString(DLL_Name))==DTL->GetSavedMD5(QString::fromStdString(DLL_Name))) //Проверяем совпадение MD5 суммы
+                {
+                    __ti2->setTextColor(Qt::red);
+                    __ti2->setToolTip(tr("Сумма MD5 файла не совпадает с суммой прописанной в БД"));
+                }
+                else
+                {
+                    __ti2->setTextColor(Qt::black);
+                    __ti2->setToolTip(tr("Сумма MD5 проверена"));
+                }            
+            }
+            else
+            {
+                __ti2->setToolTip(QString::fromStdString(DLL_Name)+" не найден");
+            }
 
-            ui.tableWidget->setColumnCount(i);
-            QTableWidgetItem* AnOneItem =new QTableWidgetItem();
-            QTableWidgetItem* AnTwoItem =new QTableWidgetItem();
-            AnOneItem->setText(QString::fromStdString(DLL_Name));
-            AnTwoItem->setText(QString::fromStdString(MD5_SUMM));
-
-            ui.tableWidget->setItem(i,0, AnOneItem);
-            ui.tableWidget->setItem(i,1, AnTwoItem);
-
-
-            //Считываем значения текущей записи для библиотеки
-
-/*            ui.tableWidget->item(i, 1)->setIcon(icon_locked);
-            ui.tableWidget->item(i, 1)->setText(tr("Н/Д"));
-            ui.tableWidget->item(i, 1)->setToolTip(tr("Атрибут определен на другом уровне Eidos"));*/
-/*
-            QFont currentFont=ui.tableWidget->item(i,0)->font();
-            currentFont.setItalic(true);
-            for (int j=0;j<2;j++)	//Создаем элементы таблицы
-                    ui.tableWidget->item(i,j)->setFont(currentFont);*/
-            i++;
+        }
+        else
+        {
+            ui.action_dll_subscribe->setDisabled(true);
+            __ti2->setToolTip(tr("Операции с файлами заблокированы"));
         }
 
-        ui.tableWidget->resizeColumnsToContents();
-        ui.tableWidget->setSortingEnabled(true);
-
-        //Устанавливаем текущим элементом первый элемент и первую видимую колонку
-        //ui.tableWidget->setCurrentCell(0,Num);
+        i++;
+    }
+    ui.tableWidget_reged_dll->resizeColumnsToContents();
+    ui.tableWidget_reged_dll->setSortingEnabled(true);
+    //Устанавливаем текущим элементом первый элемент и первую видимую колонку
+    //ui.tableWidget->setCurrentCell(0,Num);
 }
-
 
 mw_DLL_handler::~mw_DLL_handler()
 {
 
 }
+
 void mw_DLL_handler::ReadFormWidgetsAppearance()
 {
     //Процедура считывает из DbETC параметры элементов формы и устанавливает их значения
@@ -94,8 +135,8 @@ void mw_DLL_handler::WriteFormWidgetsAppearance()
     MyETC->WriteInteger("height", this->height());
     MyETC->CloseKey();
     delete MyETC;
-
 }
+
 void mw_DLL_handler::Exit()
 {
 //Выход из формы
