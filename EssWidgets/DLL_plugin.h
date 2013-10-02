8e701314db9b1ab57fdff5b47bf362f25c11317c
vladisgol <vladisgol@2acc3764-da52-0410-91d2-b5ef238ec21c>
2013-10-02 17:56:05 +0000

    Первая действующая редакция позволяющая просматривать базу

--- EssWidgets/DLL_plugin.h
+++
@@ -1,8 +0,0 @@
-#ifndef DLL_PLUGIN_H
-#define DLL_PLUGIN_H
-#include "EssentialClass.h"
-//Определение передаваемых в DLL библиотеку параметров
-typedef int (* cProc) (const QString&);
-
-
-#endif // DLL_PLUGIN_H
--- Indexer/Setup_tools/Readme.txt
+++
@@ -1,1 +0,0 @@
-Dependies: kinterbasdb
